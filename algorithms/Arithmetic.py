abcdb4f03c679c5e534691f601b80921647810f6
ArtPes <arturo.pesaro@gmail.com>
2018-06-29 12:38:11 +0200

    fix binary decode, fix some other bugs

--- algorithms/Arithmetic.py
+++ algorithms/Arithmetic.py
@@ -2,6 +2,7 @@ import decimal, time
 from decimal import Decimal
 import collections
 from prova import *
+
 decimal.getcontext().prec = 100
 
 
@@ -45,8 +46,31 @@ def filecharcount(openfile):
     return sorted(collections.Counter(c for l in openfile for c in l).items())
 
 
-def start_Arithmetic(text, file_path):
+def probabilita():
+    file = open("files_executed/dizionario.txt")
 
+    array = filecharcount(file)
+    lista_tuple = []
+
+    tot = 0
+    for (a, n) in array:
+        tot = n + tot
+
+    for (item, freq) in array:
+        lista_tuple.append((item, freq / tot))
+
+    probabilities = {}
+    low = 0
+    for (a, b) in lista_tuple:
+        high = low + b
+        word = {a: (Decimal(low), Decimal(high))}
+        probabilities.update(word)
+        low = high
+
+    return probabilities
+
+
+def start_Arithmetic(text, file_path):
     probabilities2 = {
         "a": (Decimal(0.00), Decimal(0.10)),
         "b": (Decimal(0.10), Decimal(0.15)),
